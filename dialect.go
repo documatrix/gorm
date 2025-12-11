8e3820a3b409846d3789f50d9fff1323acfc425c
FFuchsi <56107867+FFuchsi@users.noreply.github.com>
2025-12-11 09:40:49 +0100

    Update dialect.go

--- dialect.go
+++ dialect.go
@@ -74,10 +74,6 @@ func newDialect(name string, db SQLCommon) Dialect {
 		return dialect
 	}
 
-	// don't print warning for oracle
-	if name != "oracle" {
-		fmt.Printf("`%v` is not officially supported, running under compatibility mode.\n", name)
-	}
 	commontDialect := &commonDialect{}
 	commontDialect.SetDB(db)
 	return commontDialect
